db8531b8457329d3a5257f2cea9c6c4a83086475
Aranya5 <aranyabasu5@gmail.com>
2025-07-09 23:00:31 +0530

    Conditional statement

--- Test-ft/mapp-test/src/App.jsx
+++ Test-ft/mapp-test/src/App.jsx
@@ -3,14 +3,23 @@ import "./App.css";
 
 function App() {
   let foodItems = ["Pizza", "Burger", "Pasta", "Salad", "Sushi"];
+  //let foodItems = []; // Uncomment this line to test the empty state
+
   return (
     <>
+    {/* Testing map method */}
+    
       <h1>Food Items</h1>
+      {foodItems.length === 0 ? <h3>I am still hingry</h3> : null}
       <ul class="list-group">
         {foodItems.map((item) => (
           <li key={item} class="list-group-item">{item}</li>
         ))}
       </ul>
+
+
+
+
     </>
   );
 }
