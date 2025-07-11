fc38997ce01d5a1d213379a8c75b6c072ccfa2bc
Aranya5 <aranyabasu5@gmail.com>
2025-07-11 22:53:17 +0530

    Conditional statement

--- Test-ft/mapp-test/src/App.jsx
+++ Test-ft/mapp-test/src/App.jsx
@@ -8,12 +8,14 @@ function App() {
   return (
     <>
     {/* Testing map method */}
+  
     
       <h1>Food Items</h1>
-      {foodItems.length === 0 ? <h3>I am still hingry. No food Items</h3> : null}
+      {foodItems.length === 0 ? <h3>I am still hungry. No food Items</h3> : null}
       <ul class="list-group">
         {foodItems.map((item) => (
           <li key={item} class="list-group-item">{item}</li>
+
         ))}
       </ul>
 
